e822b388f69838713fdf4481bcca86739cf0daed
David Rodriguez <93050365+DavidRod1865@users.noreply.github.com>
2025-01-15 16:53:59 -0500

    second commit

--- obj/Debug/net9.0/CTRMBackend.AssemblyInfo.cs
+++ obj/Debug/net9.0/CTRMBackend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CTRMBackend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c742aa85a281070358bafb57e85dbc39a04aa006")]
 [assembly: System.Reflection.AssemblyProductAttribute("CTRMBackend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CTRMBackend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

c742aa85a281070358bafb57e85dbc39a04aa006
David Rodriguez <93050365+DavidRod1865@users.noreply.github.com>
2025-01-15 16:51:39 -0500

    first commit

---
+++ Models/Users.cs
@@ -0,0 +1,6 @@
+public class User
+{
+    public int Id { get; set; }
+    public string ClerkId { get; set; } // ID from Clerk
+    public string Role { get; set; } // Trader, Scheduler, Risk Manager
+}
